5c38dbfe3613614a9cd5615dd6aaf015570d267b
Claire Thallon <clairethallon@gmail.com>
2021-10-27 13:09:03 -0500

    updated likes on DOM

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,9 +4,10 @@ function GalleryItem(props) {
     // const [name, setName]=useState(null);
     return (
         <div>
-            <h1>GalleryItem</h1>
+            <h1>GalleryItem {props.galleryItem.id}</h1>
             {/* <p>props: {JSON.stringify(props)}</p> */}
             <img src={props.galleryItem.path} alt={props.galleryItem.description} />
+            <p>likes: </p>
         </div>
     )
 
